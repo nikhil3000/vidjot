b9fb62823400fb398ce52430b3e7f51a5daf47b2
Nikhil Yadav <nikhilyadav3000@gmail.com>
2018-07-22 19:29:26 +0530

    mlab url updated

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
 	module.exports = {
-		mongoURI: 'mongodb://nikhil:ABC123@ds245901.mlab.com:45901/vidjot-mlab'
+		mongoURI: 'mongodb://nikhil1:abc123@ds245901.mlab.com:45901/vidjot-mlab'
 	}
 }
 else
